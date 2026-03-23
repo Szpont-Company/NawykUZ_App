a72ed3f250f249e4c5bcaefbd123a2249d892bda
grzechu3o3 <jgrzesik7@icloud.com>
2026-03-23 12:36:55 +0100

    fix: removed unused function Greeting

--- app/src/main/java/com/SzpontCompany/check/MainActivity.kt
+++ app/src/main/java/com/SzpontCompany/check/MainActivity.kt
@@ -26,11 +26,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
