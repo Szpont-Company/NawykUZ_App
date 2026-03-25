bd45a828834daa8a23e39bb48e141744bf2edc23
Hubert Jaskula <hubertjaskula516@gmail.com>
2026-03-25 01:27:19 +0100

    feat(ui): implement seamless animated splash screen

--- app/src/main/java/com/SzpontCompany/check/MainActivity.kt
+++ app/src/main/java/com/SzpontCompany/check/MainActivity.kt
@@ -7,9 +7,16 @@ import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import com.SzpontCompany.check.ui.auth.LoginScreen
 import com.SzpontCompany.check.ui.theme.Cactus
 import com.SzpontCompany.check.ui.theme.CheckTheme
+import com.SzpontCompany.check.ui.auth.AnimatedSplashScreen
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import com.SzpontCompany.check.ui.theme.Crimson
+import com.SzpontCompany.check.ui.theme.Rose
 
 
 class MainActivity : ComponentActivity() {
@@ -17,20 +24,29 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-        val darkTheme = true
-        SideEffect {
-            enableEdgeToEdge(
-                statusBarStyle = if (darkTheme) {
-                    SystemBarStyle.dark(Color.TRANSPARENT)
+            val darkTheme = true
+            SideEffect {
+                enableEdgeToEdge(
+                    statusBarStyle = if (darkTheme) {
+                        SystemBarStyle.dark(Color.TRANSPARENT)
+                    } else {
+                        SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+                    }
+                )
+            }
+
+            var showSplash by remember { mutableStateOf(true) }
+
+            CheckTheme(darkTheme = darkTheme, accent = Crimson) {
+                if (showSplash) {
+                    AnimatedSplashScreen(
+                        onSplashFinished = {
+                            showSplash = false
+                        }
+                    )
                 } else {
-                    SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+                    LoginScreen()
                 }
-            )
-        }
-
-
-            CheckTheme(darkTheme = darkTheme, accent = Cactus) {
-                LoginScreen()
             }
         }
     }
