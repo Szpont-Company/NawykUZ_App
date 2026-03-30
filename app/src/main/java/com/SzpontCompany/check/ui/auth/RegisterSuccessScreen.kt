0fee4ce3c651e95e3b36750ea15a149a43c45cfa
grzechu3o3 <jgrzesik7@icloud.com>
2026-03-30 23:50:24 +0200

    feat: add empty RegisterSuccessScreen and ResetPasswordScreen components

---
+++ app/src/main/java/com/SzpontCompany/check/ui/auth/RegisterSuccessScreen.kt
@@ -0,0 +1,11 @@
+package com.SzpontCompany.check.ui.auth
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RegisterSuccessScreen(
+    onBack: () -> Unit,
+    onSuccess: () -> Unit
+) {
+
+}
---
+++ app/src/main/java/com/SzpontCompany/check/ui/auth/ResetPasswordScreen.kt
@@ -0,0 +1,11 @@
+package com.SzpontCompany.check.ui.auth
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ResetPasswordScreen(
+    onBack: () -> Unit,
+    onSuccess: () -> Unit
+) {
+
+}
