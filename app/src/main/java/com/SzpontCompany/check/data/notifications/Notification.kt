de14fdffaefa1199e8334f72b7a577c357a1c864
Hubert Jaskula <hubertjaskula516@gmail.com>
2026-05-05 01:55:54 +0200

    fix: resolve isRead mapping issue

--- app/src/main/java/com/SzpontCompany/check/data/notifications/Notification.kt
+++ app/src/main/java/com/SzpontCompany/check/data/notifications/Notification.kt
@@ -1,5 +1,6 @@
 package com.SzpontCompany.check.data.notifications
 
+import com.google.firebase.firestore.PropertyName
 import com.google.firebase.firestore.ServerTimestamp
 import java.util.Date
 
@@ -20,6 +21,8 @@ data class Notification(
     val message: String = "",
     val senderId: String? = null,
     @ServerTimestamp val createdAt: Date? = null,
-    val isRead: Boolean = false,
+    @get:PropertyName("isRead")
+    @set:PropertyName("isRead")
+    var isRead: Boolean = false,
     val relatedEntityId: String? = null
 )
